25a94c29bf09b3ebade6cf7c4623fe0fc4d1cf63
Dmitry <diman-mail@list.ru>
2022-01-31 19:52:02 +0300

    Добавил Array

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        int arr[] = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
         System.out.println( "Hello Maksim!!!" );
+
     }
 }
